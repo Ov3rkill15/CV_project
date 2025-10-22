9e79c5afc4a4e66a522620d79f9579102d8e5632
Ov3rkill15 <lwnsryd24@gmail.com>
2025-10-22 14:16:42 +0700

    tambahan di light theme

--- script.js
+++ script.js
@@ -41,4 +41,21 @@ function generateStars() {
   }
 }
 
+// Generate particles for light mode
+function generateParticles() {
+  const particlesContainer = document.getElementById('lightParticles');
+  const numberOfParticles = 30;
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    const particle = document.createElement('div');
+    particle.className = 'particle';
+    particle.style.left = Math.random() * 100 + '%';
+    particle.style.top = Math.random() * 100 + '%';
+    particle.style.animationDelay = Math.random() * 8 + 's';
+    particle.style.animationDuration = (Math.random() * 5 + 5) + 's';
+    particlesContainer.appendChild(particle);
+  }
+}
+
 generateStars();
+generateParticles();
